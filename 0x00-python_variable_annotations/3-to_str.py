3c1be9a0656c55b3a9414f6160a9c593ea32eced
vierukundo <vierukundo@gmail.com>
2024-01-14 21:10:15 +0000

    function to_str that takes a float n as argument and returns uts string representatiion

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""No imports"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to its string representation.
+
+    Parameters:
+    - n (float): The input number.
+
+    Returns:
+    str: The string representation of the input number.
+    """
+    return str(n)
