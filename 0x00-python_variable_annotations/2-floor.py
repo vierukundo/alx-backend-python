a1272d26c4899c871ad5e9b3f52cfa97919dc5cf
vierukundo <vierukundo@gmail.com>
2024-01-14 21:06:00 +0000

    function floor which takes a float n as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""import math module"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a floating-point number.
+
+    Parameters:
+    - n (float): The input number.
+
+    Returns:
+    int: The floor of the input number.
+    """
+    return math.floor(n)
