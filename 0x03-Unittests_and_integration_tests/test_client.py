485b275a02fac336bbb769f5d887ee16f7cee79d
vierukundo <vierukundo@gmail.com>
2024-02-04 22:45:13 +0000

    test for property decorated method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Module with tests for client"""
 import unittest
-from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
+from unittest.mock import patch, Mock, MagicMock, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -18,6 +18,16 @@ class TestGithubOrgClient(unittest.TestCase):
         test_instance.org
         url = test_instance.ORG_URL.format(org=org)
         mock_get_json.assert_called_once_with(url)
+
+    @patch('client.GithubOrgClient.org', return_value={"repos_url": 'url'})
+    def test_public_repos_url(self, mocked_org):
+        """Test the _public_repos_url property of GithubOrgClient."""
+        inst = GithubOrgClient('random org url')
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mocked_property:
+            mocked_property.return_value = mocked_org.return_value["repos_url"]
+            repo_url = inst._public_repos_url
+        self.assertEqual('url', repo_url)
 
 
 if __name__ == "__main__":
