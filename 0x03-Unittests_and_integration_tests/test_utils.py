915691fbb44b2016d8d4c52bbb541f374d7261e1
vierukundo <vierukundo@gmail.com>
2024-02-04 10:53:37 +0000

    Test for get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """Module for testing utils.access_nested_map"""
 import unittest
 from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, Mock
 from utils import *
 
 
@@ -32,6 +33,30 @@ class TestAccessNestedMap(unittest.TestCase):
         """test for keyError exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """test for get_json"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        # Set up the mock response and attach it to the get method
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call the get_json function
+        result = get_json(test_url)
+
+        # Assert that the get method was called exactly once with the URL
+        mock_get.assert_called_once_with(test_url)
+
+        # Assert that the output of get_json is equal to test_payload
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
